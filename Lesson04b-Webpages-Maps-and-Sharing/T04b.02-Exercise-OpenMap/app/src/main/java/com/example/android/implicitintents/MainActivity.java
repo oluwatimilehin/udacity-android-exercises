19068715e2b6a76b4036a47c8221b845a0ba8b95
Oluwatimilehin <timad97@Yahoo.com>
2017-05-05 10:29:59 +0100

    Add intent to open map

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -48,12 +49,19 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
+        String address = "6, Agoro Odiyan Street, Victoria Island, Lagos";
 
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo");
+        builder.path("0,0");
+        builder.appendQueryParameter("q", address);
+        builder.appendQueryParameter("z", "10");
 
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        Uri uri = builder.build();
+
+        Log.i(MainActivity.class.getSimpleName(), uri + " ");
+        showMap(uri);
+
     }
 
     /**
@@ -100,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
          * to view particular content. In this case, our webpage URL.
          */
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-
         /*
          * This is a check we perform with every implicit Intent that we launch. In some cases,
          * the device where this code is running might not have an Activity to perform the action
@@ -112,13 +119,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
 
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
+    void showMap(Uri uri){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
 
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
 
 
 }
